d05313b84025653ec15086e21bde68e670460788
RaRhAeu <rarha_eu@protonmail.com>
2024-08-26 12:21:43 +0200

    f

--- src/opentelemetry/instrumentation/eventiq/__init__.py
+++ src/opentelemetry/instrumentation/eventiq/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from opentelemetry.instrumentation.instrumentor import (  # type: ignore[attr-defined]
+from opentelemetry.instrumentation.instrumentor import (
     BaseInstrumentor,
 )
 
